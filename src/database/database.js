558dee77a98f03241d4360b83bc4b6605253ec0e
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-05-06 21:32:54 -0300

    alter connection string - mongo atlas

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => {
   mongoose
-    .connect('mongodb://127.0.0.1:27017/essencias-db', {
+    .connect('mongodb+srv://root:admin@api-mgtabacaria.pnnku.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
